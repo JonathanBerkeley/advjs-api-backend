07aa69a4509240f380f2ec1cf6511931904acf8a
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-12-18 12:40:03 +0000

    Updates

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+// app.use(cors())
 
 app.use('/dev', AdminRouter)
 app.use('/clan', ClanRouter)
