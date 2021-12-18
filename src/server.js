d43c4fbd24aff15f0d5f5d87e8eeba2e5e814941
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-12-18 12:52:04 +0000

    Update CORs

--- src/server.js
+++ src/server.js
@@ -11,7 +11,23 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(cors())
+
+
+//#region CORs
+// Adapted from https://stackoverflow.com/a/53437992
+const origins = ["http://localhost:3001", "https://mainuser.dev", "http://api.mainuser.dev"]
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true)
+
+        if(origins.indexOf(origin) === -1)
+            return callback(new Error("CORS unrecognized origin"), false)
+
+        return callback(null, true)
+    }
+}))
+//#endregion
 
 app.use('/dev', AdminRouter)
 app.use('/clan', ClanRouter)
