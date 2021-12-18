1ef5eaece315678b92e1086b351b4b76cf814895
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-12-18 13:01:56 +0000

    CORs

--- src/server.js
+++ src/server.js
@@ -11,27 +11,12 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-
-//#region CORs
-// Adapted from https://stackoverflow.com/a/53437992
-const origins = ["http://localhost:3001", "https://mainuser.dev", "http://api.mainuser.dev"]
-
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin) return callback(null, true)
-
-        if(origins.indexOf(origin) === -1)
-            return callback(new Error("CORS unrecognized origin"), false)
-
-        return callback(null, true)
-    }
-}))
-//#endregion
-
-app.use('/dev', AdminRouter)
-app.use('/player', PlayerRouter)
-app.use('/account', AccountRouter)
-app.use('*', IndexRouter)
+app.use('/dev', cors(), AdminRouter)
+app.use('/clan', cors(), ClanRouter)
+app.use('/player', cors(), PlayerRouter)
+app.use('/account', cors(), AccountRouter)
+app.use('*', cors(), IndexRouter)
 
 export default app
